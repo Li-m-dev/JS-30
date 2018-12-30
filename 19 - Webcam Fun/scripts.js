c96419853b7cbcfc801191ed9dfadb6f6ad057c9
Li Swafford <liswafford@yahoo.com>
2018-12-30 01:00:16 -0600

    done

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,7 +24,16 @@ function paintToCavas() {
   canvas.height = height;
 
   return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height)
+    ctx.drawImage(video, 0, 0, width, height);
+    //take pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //mess with the pixels
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+    ctx.globalAlpha = 0.1;
+    //put them back in
+    ctx.putImageData(pixels, 0, 0);
   }, 16)
 }
 
@@ -41,9 +50,54 @@ function takePhoto() {
   link.href = data;
   link.setAttribute('download', 'handsome');
   link.textContent = 'Download Image';
+  link.innerHTML = `<img src="${data}" alt="my baby"/>`
   strip.insertBefore(link, strip.firstChild);
 }
 
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 50; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; //red
+    pixels.data[i + 500] = pixels.data[i + 1]; //green
+    pixels.data[i - 550] = pixels.data[i + 2]; //blue
+
+  }
+  return pixels;
+}
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
 
 getVideo()
 
